64d136ee0b34d6615e930973bb144a17111d7506
iicmaster@VAIO-FZ <iicmaster@live.com>
2012-07-21 07:11:56 +0700

    Added upload module

---
+++ application/modules/upload/language/english/upload_lang.php
@@ -0,0 +1,11 @@
+<?php
+/*
+| -------------------------------------------------------------------
+| CMS
+| -------------------------------------------------------------------
+*/
+$lang['page_upload'] = 'Upload';
+
+
+/* End of file upload_lang.php */
+/* Location: ./application/modules/upload/language/english/upload_lang.php */
